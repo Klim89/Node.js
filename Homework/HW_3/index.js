f3f88024c4dd76242b57c8f45d68fae14c23ec3f
Dmitriy <klimov.dima@mail.ru>
2024-01-20 18:19:03 +0300

    HW_3

--- Homework/HW_3/index.js
+++ Homework/HW_3/index.js
@@ -4,3 +4,40 @@
 // — Значение счетчика необходимо сохранять в файл каждый раз, когда обновляется страница
 // — Также значение счетчика должно загружаться из файла, когда запускается обработчик страницы
 // — Таким образом счетчик не должен обнуляться каждый раз, когда перезапускается сервер.
+
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+function readCounterFromFile() {
+  try {
+    const data = fs.readFileSync("counter.json");
+    return JSON.parse(data);
+  } catch (error) {
+    return {};
+  }
+}
+
+function writeCounterToFile(counter) {
+  fs.writeFileSync("counter.json", JSON.stringify(counter, null, 2));
+}
+
+app.get("/", (req, res) => {
+  let counter = readCounterFromFile();
+  counter["/"] = (counter["/"] || 0) + 1;
+  writeCounterToFile(counter);
+
+  res.send(`<h1>Home Page</h1><a href='/about'>Link to about page</a> Просмотры: ${counter["/"]}`);
+});
+
+app.get("/about", (req, res) => {
+  let counter = readCounterFromFile();
+  counter["/about"] = (counter["/about"] || 0) + 1;
+  writeCounterToFile(counter);
+
+  res.send(`<h1>About Page</h1><a href='/'>Link to home page</a> Просмотры: ${counter["/about"]}`);
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
